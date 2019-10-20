29566ba181eb194cd79ca93ae7a6a32e583417ef
philjones <phil.jones.24.4@gmail.com>
2019-10-20 19:04:09 +0100

    WIP

--- py_tetris.py
+++ py_tetris.py
@@ -29,7 +29,8 @@ offset = 45
 start_x = WindowWidth / 2 - scale
 start_y = WindowHeight / 2 - scale - offset
 
-L = ['..X...',
+
+L = ['......',
      '..X...',
      '..X...',
      '..XX..'
@@ -44,13 +45,34 @@ O = ['......',
      '......']
 
 Z = ['......',
-     '.XX...',
      '..X...',
      '..XX..',
+     '...X..',
      '......',
      '......']
 
-piece = [L, O, Z]
+T = ['......',
+     '..X...',
+     '..XX..',
+     '..X...',
+     '......',
+     '......']
+
+I = ['......',
+     '..X...',
+     '..X...',
+     '..X...',
+     '..X...',
+     '......']
+
+S = ['......',
+     '...X..',
+     '..XX..',
+     '..X...',
+     '......',
+     '......']
+
+piece = [L, O, Z, T, I, S]
 
 
 def reset_game():
@@ -70,17 +92,42 @@ def play_field_grid(col, row, surface):
             pygame.draw.line(surface, (RED), (sx + j * 30, sy),
                              (sx + j * 30, sy + play_height))  # vertical lines
 
-def draw_piece(surface, select):
-    sx = top_left_x
-    sy = top_left_y
+def draw_piece(surface, select, x, y, start):
+    if start:
+        sx = top_left_x
+        sy = top_left_y
+    else:
+        sx = x
+        sy = y
     for i in (range(5)):
         for j in (range(5)):
             if piece[select][j][i] == "X":
-                #pygame.draw.circle(surface, WHITE, (int(start_x + scale * i), int(start_y + scale * j)), int(scale / 3))
                 pygame.draw.rect(surface, BLUE, (sx + i * scale / 3, sy + j * scale / 3, scale / 3 - 2, scale / 3 - 2))
     # Update the screen
     pygame.display.flip()
 
+def update_play_field(surface, font, font2):
+    # Update screen
+    tetris_surface.fill(BLACK)
+    # Redraw Grid
+    play_field_grid(10, 20, tetris_surface)
+    # Update Display for user
+    text = font.render("SCORE " + str(tetris_lines), True, WHITE)
+    text2 = font.render("LEVEL " + str(level), True, WHITE)
+    if game_over:
+        text = font.render("SCORE " + str(tetris_lines), True, WHITE)
+        text2 = font.render("LEVEL " + str(level), True, WHITE)
+        text_game_over = font2.render("GAME OVER!!! SPACE TO RESTART..", True, RED)
+        tetris_surface.blit(text_game_over, [80, WindowHeight - 400])
+    pygame.draw.line(tetris_surface, WHITE, (0, WindowHeight - 65), (WindowWidth, WindowHeight - 65))
+    tetris_surface.blit(text, [20, WindowHeight - 60])
+    tetris_surface.blit(text2, [WindowWidth - 190, WindowHeight - 60])
+    # Update the screen
+    pygame.display.flip()
+
+def select_piece():
+    piece_select = random.randrange(0, 5)
+    return piece_select
 
 def main():
     reset_game()
@@ -92,39 +139,42 @@ def main():
     # Initialise fonts we will use
     font = pygame.font.SysFont('Arial', 50, False, False)
     font2 = pygame.font.SysFont('Arial', 25, False, False)
+    count = 0
+
+    # Initialise the display
+    x = start_x
+    y = start_y
+    update_play_field(tetris_surface, font, font2)
+    draw_piece(tetris_surface, select_piece(), x, y, False)
+
 
 
     while loop:
-
-        # Update screen
-        tetris_surface.fill(BLACK)
-
-        # Redraw Grid
-        play_field_grid(10, 20, tetris_surface)
-        draw_piece(tetris_surface, 0)
         # Control FPS
         clock.tick(frame_rate)
 
-        # Update Display for user
-        text = font.render("SCORE " + str(tetris_lines), True, WHITE)
-        text2 = font.render("LEVEL " + str(level), True, WHITE)
-        if game_over:
-            text = font.render("SCORE " + str(tetris_lines), True, WHITE)
-            text2 = font.render("LEVEL " + str(level), True, WHITE)
-            text_game_over = font2.render("GAME OVER!!! SPACE TO RESTART..", True, RED)
-            tetris_surface.blit(text_game_over, [80, WindowHeight - 400])
-        pygame.draw.line(tetris_surface, WHITE, (0, WindowHeight - 65), (WindowWidth, WindowHeight - 65))
-        tetris_surface.blit(text, [20, WindowHeight - 60])
-        tetris_surface.blit(text2, [WindowWidth - 190, WindowHeight - 60])
-
-        # Update the screen
-        pygame.display.flip()
-
+        # Event handler
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    update_play_field(tetris_surface, font, font2)
+                    draw_piece(tetris_surface, select_piece(), x, y, False)
 
-        keys = pygame.key.get_pressed()
+                if event.key == pygame.K_RIGHT:
+                        x = x + scale / 3
+                        update_play_field(tetris_surface, font, font2)
+                        draw_piece(tetris_surface, count, x, y, False)
+                if event.key == pygame.K_LEFT:
+                        x = x - scale / 3
+                        update_play_field(tetris_surface, font, font2)
+                        draw_piece(tetris_surface, count, x, y, False)
+                if event.key == pygame.K_DOWN:
+                        y = y + scale / 3
+                        update_play_field(tetris_surface, font, font2)
+                        draw_piece(tetris_surface, count, x, y, False)
+
 
 
 # Call main
